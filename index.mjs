30c510cb1360723af0b5f2b095a8bcd9601c11e3
Cong Nguyen <kaom.n.92@gmail.com>
2022-08-06 11:46:51 +0300

    Push

--- index.mjs
+++ index.mjs
@@ -2,14 +2,13 @@ import { roll } from "./roll.mjs";
 import * as dotenv from "dotenv" // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 import DiscordJS, { Client, GatewayIntentBits} from "discord.js"
 dotenv.config()
+// New Discord Client
 const client = new DiscordJS.Client({
-	intents: [
-		GatewayIntentBits.Guilds,
-		GatewayIntentBits.GuildMessages,
-		GatewayIntentBits.MessageContent
+	// intents/permissions for the bot
+	intents: [ GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent
 	]
 });
-
+// write on stdout/console when bot is logged in
 client.on("ready", () => {
 	console.log(`Logged in as ${client.user.tag}`);
 });
