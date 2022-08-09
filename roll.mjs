2c92f40b17c6cba4175d137cf97fcfe486a82c62
Cong Nguyen <kaom.n.92@gmail.com>
2022-08-09 10:20:51 +0300

    Push

--- roll.mjs
+++ roll.mjs
@@ -1,10 +1,8 @@
+import crypto from "crypto"
 // Returns a random value between min(inclusive) and max(inclusive max).
 // min >= 1 && max <= Number.MAX_SAFE_INTEGER
-import crypto from "crypto"
-
 function getRandomIntBetween(min, max) {
 	return crypto.randomInt(min, max + 1)
-	//return Math.floor(Math.random() * (max - min + 1) + min);
 }
 // Returns a random value between 1 and input max(inclusive).
 function getRandomInt(max) {
@@ -13,7 +11,6 @@ function getRandomInt(max) {
 // Returns a random value between 1 and 100.
 function getRandomInt100() {
 	return crypto.randomInt(1, 101);
-	//return Math.floor(Math.random() * (100)) + 1;
 }
 // Check if value is a number
 function isNum(val){
