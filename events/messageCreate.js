a7f374eabf22e35e8be37fa0a89cbcf9ead0a965
Cong Nguyen <kaom.n.92@gmail.com>
2022-08-14 00:01:48 +0300

    Modified code

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,17 +1,19 @@
 module.exports = (client, message) => {
 	const prefix = "!"
 	// Ignore all bots
-	if (message.author.bot) return;
+	if (message.author.bot)
+		return;
 	// Ignore messages not starting with the prefix
 	if (!message.content.startsWith(prefix))
 		return;
-	// Our standard argument/command name definition.
+	// Remove the prefix and get the arguments form the message.
 	const args = message.content.slice(prefix.length).trim().split(/ +/g);
+	// Get the command from the args array.
 	const command = args.shift().toLowerCase();
 	// Grab the command data from the client.commands Enmap
 	const cmd = client.commands.get(command);
-	// If that command doesn't exist, silently exit and do nothing
+	// If command doesn't exist, exit and do nothing
 	if (!cmd) return;
 	// Run the command
-	cmd.run(message, args, command);
+	cmd.run(message, args);
   };
