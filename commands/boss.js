207486bc8f1df6a9ac713f51b04b4f3a71752ea6
Cong Nguyen <kaom.n.92@gmail.com>
2023-07-09 11:56:37 +0300

    Added boss command to show next boss spawn

---
+++ commands/boss.js
@@ -0,0 +1,39 @@
+/*
+	Shows Diablo 4 World boss next spawn time.
+	!boss.
+*/
+const { codeBlock } = require("@discordjs/builders");
+const { getEvents } = require("../helpers/axios.js");
+const { yellow, blue } = require("../helpers/colors.js");
+
+exports.run = async (message, args) => {
+	var output = ""
+	var content = "ansi";
+
+	data = await getEvents()
+	var current = new Date()
+	current = current.getTime()/1000
+	hours = Math.floor((data.boss.expected - current)/3600)
+	minutes = Math.floor(((data.boss.expected - current) - (hours*3600))/60)
+	seconds = Math.floor(((data.boss.expected - current) - (hours*3600) - (minutes*60)))
+
+	output += `${blue}Next World Boss Spawn: ${yellow}${hours}h ${minutes}m ${seconds}s\n`
+	output += `${blue}Boss: ${yellow}${data.boss.name}\n`
+	output += `${blue}Zone: ${yellow}${data.boss.zone}\n`
+	output += `${blue}Location: ${yellow}${data.boss.territory}\n`
+
+	message.channel.send(codeBlock(content, output));
+	return;
+}
+
+exports.help = {
+	name: "boss",
+	description: "\u001b[0;37mShows next World boss spawn time.",
+	usage: "\u001b[0;37m!\u001b[0;32mboss"
+};
+
+exports.helpMobile = {
+	name: "boss",
+	description: "Shows next World boss spawn time..",
+	usage: "!boss"
+};
