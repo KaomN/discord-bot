f64152e9b70fa4874e6c2305f607257916220ee6
Cong Nguyen <kaom.n.92@gmail.com>
2023-07-15 17:11:15 +0300

    Modified

--- helpers/colors.js
+++ helpers/colors.js
@@ -4,6 +4,6 @@ var white = "\u001b[0;37m";
 var yellow = "\u001b[0;33m";
 var red = "\u001b[0;31m";
 var blue = "\u001b[0;34m";
-var noColor = "\u001b[0m"
+var noColor = "\u001b[0m";
 
 module.exports = { green, cyan, white, yellow, red, blue, noColor};
